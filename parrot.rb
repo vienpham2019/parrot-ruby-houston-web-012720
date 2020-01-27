ef8b41a60b3fa7869758f673840c253463ee22af
vien pham <vienpham2019@gmail.com>
2020-01-27 19:56:05 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string= "Squawk!"){
+def parrot(string = "Squawk!"){
   return string
 }
 parrot("hello")
